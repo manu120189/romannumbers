35f6de8f0ee5e0d89cb00dfe25e314dbb39d2fad
Emmanuel <emmanueljpo@gmail.com>
2019-11-10 13:23:21 -0400

    Adding app files

---
+++ romanDemo/demoApp/services.py
@@ -0,0 +1,12 @@
+# dict.put('I', 1);
+# dict.put('V', 5);
+# dict.put('X', 10);
+# dict.put('L', 50);
+# dict.put('C', 100);
+# dict.put('D', 500);
+# dict.put('M', 1000);
+
+
+def integer_to_roman(integer_value = 0):
+
+    return
--- romanDemo/demoApp/views.py
+++ romanDemo/demoApp/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 
+from .services import integerToRoman
+
 
 def index(request):
-    return HttpResponse("Demo app!")
+    return HttpResponse(integerToRoman(1))
